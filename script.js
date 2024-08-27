5b8ef30225c9b8e32e4d3c29e1d46175b806d6e3
giteshpandit <panditgitesh38@gmail.com>
2024-08-27 18:12:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
+// script.js
 
+function getFormvalue() {
+    // Access the form element
+    var form = document.getElementById("form1");
+    
+    // Retrieve the first name and last name values
+    var firstName = form.elements["fname"].value;
+    var lastName = form.elements["lname"].value;
+    
+    // Display the full name in an alert
+    alert(firstName + " " + lastName);
 }
+
